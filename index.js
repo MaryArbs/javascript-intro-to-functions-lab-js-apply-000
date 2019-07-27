d37dbb6289247ac0fcc05c0d02a89a4d2a3fa405
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-07-27 16:17:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout (string) {
+  return string. toUpperCase()
+  }
+function whisper (string) {
+  return string. toLowerCase()
+}
+function sayHiToGrandma (string){
+  if (string. toLowerCase) {
+    return "I can't hear you!"}
+}
+function sayHiToGrandma (string) {
+  if (string. toUpperCase) {
+    return "YES INDEED!"}
+}
+function sayHiToGrandma (string){
+if (string. IloveyouGrandma) {
+  return "I love you, too."
+}
+}
