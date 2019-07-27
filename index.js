95912630c5919b9129c3b474de1d507a3472d186
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-07-27 16:59:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper (string) {
   console.log (string. toLowerCase())
 }
 function sayHiToGrandma(string) {
-  var 
+  var
 }{
   if (string. toLowerCase){
     return "I can't hear you!"
